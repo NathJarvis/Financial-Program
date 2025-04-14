ba95d95e8223acab2c8c0997da4f8937ba3bf807
Nathan Jarvis <32169516+NathJarvis@users.noreply.github.com>
2025-04-14 19:18:20 +0100

    Update FinancialProgram.py
    chnaged the code to add the added features

--- FinancialProgram.py
+++ FinancialProgram.py
@@ -1,8 +1,12 @@
 #import packs
 from datetime import datetime
+expenselist = []
+incomelist = []
+viewexplist = []
+viewinclist = []
 
 #function defining
-#Age Verifier
+#Age Verifier which is an added feature to make the program different
 def agecheck():
     print()
     print("We need to confirm you are over 18 years old to continue.")
@@ -18,13 +22,14 @@ def agecheck():
             exit()
     else:
         print()
-        print("You have not entered a valid age, please try again.")
+        print("You have not entered a valid input, please try again.")
         agecheck()
 
 #T&C Verifier
+#Verifier which is an added feature to make the program different
 def termscheck():
     print("Please read and agree to the terms and conditions of the app.")
-    print("You can find them here: https://www.example.com/termsandconditions")
+    print("You can find them here: https://www.FinancialProgram.com/t&Cs")
     print()
     agree = input("Do you agree to the terms and conditions? (Y/N) ")
     if agree.upper() == "Y":
@@ -35,7 +40,7 @@ def termscheck():
         print("You have not agreed to the terms and conditions, we can therefore not allow you to continue. Please try again later!")
         exit()
 
-#Front Page function
+#Front Page function allowing the user to select what they want to do, allows the user to exit the program.
 def frontpage():
     print("What would you like to do today?")
     print()
@@ -45,18 +50,29 @@ def frontpage():
     print()
     print("3  Work out VAT on an Item (UK only)")
     print()
-    answer = int(input("Answer (1, 2 ,3): "))
+    print("4. View totals and comparisons")
+    print()
+    print("5. Exit the app")
+    print()
+    answer = int(input("Answer (1, 2, 3, 4, 5): "))
     if answer == 1:
         incomeoptions()
     elif answer == 2:
         expenseoptions()
     elif answer == 3:
         VATUK()
+    elif answer == 4:
+        comparisons()
+    elif answer == 5:
+        print("Thank you for using our service, we hope you enjoyed it!")
+        print("Goodbye!")
+        exit()
     else:
         print()
         print("You have not entered a valid answer, please try again.")
         frontpage()
 
+#income Options Page
 def incomeoptions():
     print("Here are your income options:")
     print()
@@ -82,6 +98,7 @@ def incomeoptions():
         print("You have not entered a valid answer, please try again.")
         incomeoptions()
 
+#Expense Options Page
 def expenseoptions():
     print("Here are your expense options:")
     print()
@@ -107,48 +124,179 @@ def expenseoptions():
         print("You have not entered a valid answer, please try again.")
         expenseoptions()
 
+#Main Body of the add an income function, contains nested If statements, while loops both nested and un-nested,
+#for loops and try except for the date feature
 def incomecalc():
     global incomelist
-    incomelist = []
+    global newincome
+    global viewinclist
     while True:
+        print()
         addincome = input("Add an income, Press Y to conitnue or N to go back. ")
+        print()
         if addincome.upper() == "Y":
             newincome = {}
-            date = input("Enter the date of the income (dd-mm-yyyy): ")
+            date = input("Enter the date of the income (dd/mm/yyyy): ")
+            print()
             try:
-                date = datetime.strptime(date, "%d-%m-%Y")
+                date = datetime.strptime(date, "%d/%m/%Y")
             except ValueError:
                 print("You have not entered a valid date, please try again.")
                 incomecalc()
-            newincome["date"] = date.strftime("%d-%m-%Y")
-            newincome["type"] = input("Enter the type of income: ")
-            newincome["amount"] = input("Enter the amount of the income: ")
+            newincome["date"] = date.strftime("%d/%m/%Y")
+            print("1. Active income (Wage, Salary, Benefits, etc.)")
+            print("2. Portfolio income (Interest, Dividends, etc.)")
+            print("3. Passive income (Investments, Rental, etc.)")
+            print("4. Other income (Gifts, Savings, etc.)")
+            print()
+            newincometype= input("Please select from the list the type of income"
+                                      " and insert the number: ")
+            print()
+            while True:
+                if newincometype == "1":
+                    newincome["type"] = "Active income"
+                    break
+                elif newincometype == "2":
+                    newincome["type"] = "Portfolio income"
+                    break
+                elif newincometype == "3":
+                    newincome["type"] = "Passive income"
+                    break
+                elif newincometype == "4":
+                    newincome["type"] = "Other income"
+                    break
+                else:
+                    print("You have not entered a valid number, please try again.")
+                    newincometype = input("Please select from the list the type of income"
+                                      "and insert the number: ")
+                    print()
+            newincome["amount"] = input(float("Enter the amount of the income: "))
+            print()
             incomelist.append(newincome)
-            print(f"Income added successfully! This is your entry: {newincome}")
+            viewinclist.append(newincome.get("date"))
+            viewinclist.append(newincome.get("type"))
+            viewinclist.append(newincome.get("amount"))
+            print(f"Income added successfully! This is your entry: {newincome.values()}")
         elif addincome.upper() == "N":
             print("You have chosen not to add an income.")
             print(f"Here is your list of incomes: {incomelist}")
-            frontpage()
+            incomeoptions()
+            break
+        else:
+            print("You have not entered a valid input, please use Y or N.")
+            print()
+
+#Main Body of the add an expense function, contains same loops and if statements as the income section, also contains
+#the try and except for the date feature
+def expensecalc():
+    global expenselist
+    global newexpense
+    global viewexplist
+    while True:
+        print()
+        addexpense = input("Add an Expense, Press Y to conitnue or N to go back. ")
+        print()
+        if addexpense.upper() == "Y":
+            newexpense = {}
+            date = input("Enter the date of the Expense (dd/mm/yyyy): ")
+            try:
+                date = datetime.strptime(date, "%d/%m/%Y")
+            except ValueError:
+                print()
+                print("You have not entered a valid date, please try again.")
+                print()
+                expensecalc()
+            newexpense["date"] = date.strftime("%d/%m/%Y")
+            print("1. Housing expenses (Rent, Mortgage, Loan, etc.)")
+            print("2. Utilities expenses (Electricity, Water, Gas, etc.)")
+            print("3. Food expenses (Food, Drinks, Restaurants, etc.)")
+            print("4. Transport expenses (Car, Travel, Airplane, etc.)")
+            print("5. Entertainment expenses (Movies, TV, Music, etc.)")
+            print("6. Education expenses (School, University, etc.)")
+            print("7. Health expenses (Medical, Dental, Blood, etc.)")
+            print("8. Other expenses (Clothes, Subscriptions, etc.)")
+            print()
+            newexpensetype = input("Please select from the list the type of expense"
+                                  " and insert the number: ")
+            while True:
+                if newexpensetype == "1":
+                    newexpense["type"] = "Housing expenses"
+                    break
+                elif newexpensetype == "2":
+                    newexpense["type"] = "Utilities expenses"
+                    break
+                elif newexpensetype == "3":
+                    newexpense["type"] = "Food expenses"
+                    break
+                elif newexpensetype == "4":
+                    newexpense["type"] = "Transport expenses"
+                    break
+                elif newexpensetype == "5":
+                    newexpense["type"] = "Entertainment expenses"
+                    break
+                elif newexpensetype == "6":
+                    newexpense["type"] = "Education expenses"
+                    break
+                elif newexpensetype == "7":
+                    newexpense["type"] = "Health expenses"
+                    break
+                elif newexpensetype == "8":
+                    newexpense["type"] = "Other expenses"
+                    break
+                else:
+                    print("You have not entered a valid number, please try again.")
+                    newincometype = input("Please select from the list the type of expenses"
+                                          "and insert the number: ")
+            newexpense["amount"] = input(float("Enter the amount of the expense: "))
+            expenselist.append(newexpense)
+            viewexplist.append(newexpense.get("date"))
+            viewexplist.append(newexpense.get("type"))
+            viewexplist.append(newexpense.get("amount"))
+            print(f"Expense added successfully! This is your entry: {newexpense}")
+        elif addexpense.upper() == "N":
+            print("You have chosen not to add an expense.")
+            print(f"Here is your list of expense: {expenselist}")
+            expenseoptions()
             break
         else:
             print("You have not entered a valid input, please use Y or N.")
 
+#View The whole list of incomes
 def viewincome():
-    print(incomelist)
-    incomeoptions()
+    if viewinclist == []:
+        print("You have not added any incomes yet, please go back and add an income before trying again!")
+        incomeoptions()
+    else:
+        print(viewinclist)
+        incomeoptions()
 
+#View The whole list of expenses
 def viewexpense():
-    print(expenselist)
-    expenseoptions()
+    if viewexplist == []:
+        print("You have not added any expenses yet, please go back and add an expense before trying again!")
+        expenseoptions()
+    else:
+        print(viewexplist)
+        expenseoptions()
 
+#Main Body of the delete an income function, uses the try and except for the date feature as verification.
+#uses a for loop to run through an unlimted number of incomes to search for the correct dictionary to delete
 def deleteincome():
-    print("Please enter the date of the income you would like to delete.")
-    deldate = input("Date dd-mm-yyyy: ")
+    if incomelist == []:
+        print("You have not added any incomes yet, please go back and add an income before trying again!")
+        incomeoptions()
+    else:
+        print("Please enter the date of the income you would like to delete.")
+    print()
+    deldate = input("Date dd/mm/yyyy: ")
+    print()
     delinc = input("How much was the income for that you want to delete?")
+    print()
     try:
-        date = datetime.strptime(deldate, "%d-%m-%Y")
+        date = datetime.strptime(deldate, "%d/%m/%Y")
     except ValueError:
         print("You have not entered a valid date, please try again.")
+        print()
         deleteincome()
     for newincome in incomelist:
         if newincome["date"] == deldate and newincome["amount"] == delinc:
@@ -156,12 +304,14 @@ def deleteincome():
             print(f"Income deleted successfully! This is your remaining Incomes: {incomelist}")
             break
 
+#Main Body of the delete an Expense function, uses the try and except for the date feature as verification.
+#uses a for loop to run through an unlimted number of expenses to search for the correct dictionary to delete
 def deleteexp():
     print("Please enter the date of the expense you would like to delete.")
-    deldate = input("Date dd-mm-yyyy: ")
+    deldate = input("Date dd/mm/yyyy: ")
     delexp = input("How much was the income for that you want to delete?")
     try:
-        date = datetime.strptime(deldate, "%d-%m-%Y")
+        date = datetime.strptime(deldate, "%d/%m/%Y")
     except ValueError:
         print("You have not entered a valid date, please try again.")
         deleteexp()
@@ -171,33 +321,9 @@ def deleteexp():
             print(f"Expense deleted successfully! This is your remaining Expenses: {expenselist}")
             break
 
-
-def expensecalc():
-    global expenselist
-    expenselist = []
-    while True:
-        addexpense = input("would you like to add an expense? (Y/N) ")
-        if addexpense.upper() == "Y":
-            newexpense = {}
-            date = input("Enter the date of the Expense (dd-mm-yyyy): ")
-            try:
-                date = datetime.strptime(date, "%d-%m-%Y")
-            except ValueError:
-                print("You have not entered a valid date, please try again.")
-                incomecalc()
-            newexpense["date"] = date.strftime("%d-%m-%Y")
-            newexpense["type"] = input("Enter the type of expense: ")
-            newexpense["amount"] = input("Enter the amount of the expense: ")
-            expenselist.append(newexpense)
-            print(f"Expense added successfully! This is your entry: {newexpense}")
-        elif addexpense.upper() == "N":
-            print("You have chosen not to add an expense.")
-            print(f"Here is your list of expense: {expenselist}")
-            frontpage()
-            break
-        else:
-            print("You have not entered a valid input, please use Y or N.")
-
+#An added function to make the program different, lets the user calculate the amount of tax on an item for sale.
+#only used for UK purposes and provides information on the current tax rate, price before vat, price after vat
+#and the total amount of VAT on the item
 def VATUK():
     UKSalesTax = 20
     print()
@@ -214,6 +340,37 @@ def VATUK():
     print()
     exit()
 
+#Main body of the comparisons function, allows the user to view the sum of all the incomes put together, view the sum
+#of all the expenses put together, work out the remaining balance after total income is taken away from total expenses.
+#float() was added as if the user entered the amount as a decimal the program would crash when working out totals.
+def comparisons():
+    totalincome = sum(float(newincome["amount"]) for newincome in incomelist)
+    totalexpense = sum(float(newexpense["amount"]) for newexpense in expenselist)
+    while True:
+        print("What would you like to know or compare?")
+        print()
+        print("1. Total Income Amount")
+        print()
+        print("2. Total Expense Amount")
+        print()
+        print("3. Total Leftover (Total Income - Total Expense)")
+        print()
+        print("4. Return to the main menu.")
+        print()
+        compans = input("What would you like to do? (1, 2, 3, 4): ")
+        if compans == "1":
+            print(f"Total income amount is {totalincome}")
+        elif compans == "2":
+            print(f"Total expense amount is {totalexpense}")
+        elif compans == "3":
+            totalleftover = totalincome - totalexpense
+            print(f"Total leftover is {totalleftover}")
+        elif compans == "4":
+            frontpage()
+            break
+        else:
+            print()
+            print("You have not entered a valid answer, please try again.")
 
 #Introduction
 print("     Welcome to Your Finances!")
@@ -226,13 +383,9 @@ name = name.title()
 print()
 print(f"Hello {name}, Nice to meet you!")
 
-#Check user is above 18 and agrees with terms and conditions of the app
+#runs the functions that Checks user is above 18 and agrees with terms and conditions of the app
 agecheck()
 termscheck()
 
-#front page of program
+#front page of program, starts the main program which interconnects all functions.
 frontpage()
-
-
-
-
